e73f9d40c8e3c4fc42c51e18f1aeb59828bcd506
LoganDawes <logantdawes@gmail.com>
2025-03-31 10:28:43 -0500

    Create parsing.cpp, testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,9 @@
+/*
+Group B
+Author: Logan Dawes
+Email: logan.dawes@okstate.edu
+Date: 04/05/2025
+
+Description: Generates a random initialization of intersections.txt and trains.txt for automated testing.
+*/
+
