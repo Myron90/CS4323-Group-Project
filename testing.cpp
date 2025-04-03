126f0c4315525e805959162b1a19dffc34f64b21
LoganDawes <logantdawes@gmail.com>
2025-04-03 13:51:16 -0500

    Generate config files function

--- testing.cpp
+++ testing.cpp
@@ -11,16 +11,22 @@ Description: Generates a random initialization of intersections.txt and trains.t
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <string>
+#include <set>
 
-void generateIntersections() {
+void generateConfig() {
     // File: intersections.txt
-    std::ofstream file("intersections.txt");
+    std::ofstream intersectionsFile("intersections.txt");
 
     // Random Seed
     std::srand(std::time(0));
 
     // Generate a 1-10 intersections
     int numIntersections = (std::rand() % 10) + 1;
+
+    // Vector for intersections
+    std::vector<std::string> intersections;
 
     for (int i = 0; i < numIntersections; ++i) {
         // Generate 1-3 capacity per intersection
@@ -29,8 +35,63 @@ void generateIntersections() {
         // Set intersection number as alphabetical
         char alpha = 'A' + i;
 
+        // Add intersection to vector
+        std::string intersectionName = strncat("Intersection", &alpha, 1);
+        intersections.push_back(intersectionName);
+
         // Write to file
-        file << "Intersection" << alpha << ':' << capacity << std::endl;
+        intersectionsFile << intersectionName << ':' << capacity << std::endl;
     }
-    file.close();
+    intersectionsFile.close();
+
+    // File: trains.txt
+    std::ofstream trainsFile("trains.txt");
+
+    // Random Seed
+    std::srand(std::time(0));
+
+    // Generate a 1-10 trains
+    int numTrains = (std::rand() % 10) + 1;
+
+    for (int i = 0; i < numTrains; ++i) {
+        // Generate random intersection number for route
+        int numRouteIntersections = (std::rand() % numIntersections) + 1;
+
+        // Set for route intersections
+        std::set<std::string> route;
+
+        // Write to file
+        trainsFile << "Train" << i+1 << ':';
+
+        // Generate Route
+        for (int j = 0; j < numRouteIntersections; ++j) {
+            std::string selectedIntersection;
+            do {
+                // Aquire random intersection index to select intersection
+                int intersectionIndex = std::rand() % numIntersections;
+                selectedIntersection = intersections[intersectionIndex];
+            }
+            // Go through route to ensure no duplicates
+            while (route.find(selectedIntersection) != route.end());
+
+            // Add to route
+            route.insert(selectedIntersection);
+
+            // Comma seperated entries
+            if (j != 0) {
+                trainsFile << ",";
+            }
+
+            // Write route
+            trainsFile << selectedIntersection;
+        }
+        trainsFile << std::endl;
+
+    }
+    trainsFile.close();
+}
+
+int main(){
+    generateConfig();
+    return 0;
 }
