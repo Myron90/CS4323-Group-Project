414227f91dd1c41e6bf22d0e6aed8d9b4341b65d
LoganDawes <logantdawes@gmail.com>
2025-04-03 13:08:22 -0500

    Generate intersections function

--- testing.cpp
+++ testing.cpp
@@ -7,3 +7,30 @@ Date: 04/05/2025
 Description: Generates a random initialization of intersections.txt and trains.txt for automated testing.
 */
 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+void generateIntersections() {
+    // File: intersections.txt
+    std::ofstream file("intersections.txt");
+
+    // Random Seed
+    std::srand(std::time(0));
+
+    // Generate a 1-10 intersections
+    int numIntersections = (std::rand() % 10) + 1;
+
+    for (int i = 0; i < numIntersections; ++i) {
+        // Generate 1-3 capacity per intersection
+        int capacity = (std::rand() % 3) + 1;
+
+        // Set intersection number as alphabetical
+        char alpha = 'A' + i;
+
+        // Write to file
+        file << "Intersection" << alpha << ':' << capacity << std::endl;
+    }
+    file.close();
+}
