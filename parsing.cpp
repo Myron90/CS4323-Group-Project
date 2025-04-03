105c8eeb0aaf22204e28296be1a333dcde110586
eve-wils <evelyn.wilson@okstate.edu>
2025-04-03 12:24:56 -0500

    Moved is_mutex definition to the constructor, corrected the type of parseTrains output, created route vector in parseTrains and added the intersections to it. Created train objects

--- parsing.cpp
+++ parsing.cpp
@@ -24,10 +24,10 @@ class Intersection {
 public:
     string name;
     unsigned int capacity;
-    bool is_mutex = capacity == 1 ? true : false;
+    bool is_mutex;
 
     // Constructor
-    Intersection(string name, unsigned int capacity) : name(name), capacity(capacity) {}
+    Intersection(string name, unsigned int capacity) : name(name), capacity(capacity), is_mutex(c==1) {}
     
     // TODO: define Trains vector 
     // TODO: Define mutex and semaphore functions to add and remove trains
@@ -70,30 +70,35 @@ unordered_map<string, Intersection*> parseIntersections(const string& filename){
     return intersections;
 }
 
-unordered_map<string, vector<Intersection*>> parseTrains(const string& filename, unordered_map<string, Intersection*>& parseIntersections){
- ifstream file(filename);
- string line;
+unordered_map<string, Train*> parseTrains(const string& filename, unordered_map<string, Intersection*>& intersections){
+    ifstream file(filename);
+    string line;
+    unordered_map<string, Train*> trains;
  
- while(getline(file, line)) { // While there is a next line
-     stringstream ss(line);
-     string name;
-     string intersection1;
-     string intersection2;
-     string intersection3;
-     int count = 0;
+    while(getline(file, line)) { // While there is a next line
+        stringstream ss(line);
+        string name;
+        string intersection1, intersection2, intersection3;
+        vector<Intersection*> route;
 
-     getline(ss, name, ',');
-     getline(ss, intersection1, ',');
-     getline(ss, intersection2, ',');
-     ss >> intersection3;
+        getline(ss, name, ',');
+        getline(ss, intersection1, ',');
+        getline(ss, intersection2, ',');
+        ss >> intersection3;
 
-     cout << "Name : " << name << " , Intersections: " << intersection1 << ", " << intersection2 << ", and " << intersection3 << endl;
-     // TODO: read intersections into a route and verify that they exist in the intersections map
- }
+        cout << "Name : " << name << " , Intersections: " << intersection1 << ", " << intersection2 << ", and " << intersection3 << endl;
+        // Read intersections into a route, pulling from the intersections unordered map
+        route.push_back(intersections[intersection1]);
+        route.push_back(intersections[intersection2]);
+        route.push_back(intersections[intersection3]);
 
+        trains[name] = new Train(name, route);
+    }
+    
+    return trains;
 }
 
 int main() { 
     unordered_map<string, Intersection*> intersections = parseIntersections("intersections.txt");
-    parseTrains("trains.txt", intersections);
+    unordered_map<string, Train*> trains = parseTrains("trains.txt", intersections);
 }
