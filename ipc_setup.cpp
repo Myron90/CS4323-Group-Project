b883fdaa13c3bdfe468e50ef10b0981dde2c4f7d
Rickyp4 <123599174+Rickyp4@users.noreply.github.com>
2025-04-03 21:08:26 -0500

    Update ipc_setup.cpp

--- ipc_setup.cpp
+++ ipc_setup.cpp
@@ -12,3 +12,61 @@ Description:
 // Create message queues
 
 // Store the mutexes and semaphores, set the values to match specified capacities
+
+/*
+Group B
+Author: Richard Powers
+Email: richard.w.powers@okstate.edu
+Date: 4/1/2025
+
+Description: 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/msg.h>
+
+#define shm_name "/shared_mem"
+#define mq_name "/msg_queue"
+#define size sizeof(int)
+
+struct msg_request{
+    long mtype;
+    int train_id;
+    char intersection[50];
+} message;
+
+int main() {
+    
+    // Shared Memory
+    const int size = 1024;
+    int shm_fd;
+
+    long long int * ptr;
+
+    shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666); // Create shared memory
+
+    ftruncate(shm_fd, size); // Set memory size
+    ptr = mmap(0, size, PROT_WRITE, MAP_SHARED, shm_fd, 0); // Shared memory pointer
+
+    // Message Queue
+    key_t key;
+    int msgid;
+
+    key = ftok(mq_name, mtype);
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+
+    msgrcv(msgid, &message, sizeof(message), mtype, 0);
+
+    msgctl(msgid, IPC_RMID, NULL);
+
+    return 0;
+}
